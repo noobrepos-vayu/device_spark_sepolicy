ea527ab578be0b97fe4fb484650f9f22cfe3d8b5
Ayush Dubey <ayushdubey70@gmail.com>
2020-09-29 20:32:26 +0530

    Spark: remove lineage trust sepolicy
    *Afaik we don't build lineage trust hal
    
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -23,8 +23,7 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/spark/sepolicy/common/dynamic \
-    device/spark/sepolicy/common/system
+    device/spark/sepolicy/common/dynamic
 else
 BOARD_SEPOLICY_DIRS += \
     device/spark/sepolicy/common/dynamic \
